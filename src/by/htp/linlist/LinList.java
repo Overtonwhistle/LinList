64e583e9ce6d3ce051fb6b182aeba3a26478ce0d
Pavel Sorokoletov <overton.whistle@gmail.com>
2017-03-26 13:54:55 +0300

    Task2

---
+++ src/by/htp/linlist/LinList.java
@@ -0,0 +1,168 @@
+package by.htp.linlist;
+
+class Node<E> {
+	public E value;
+	public Node<E> next;
+
+	public Node(E e) {
+		value = e;
+	};
+}
+
+public class LinList<E> {
+	protected Node<E> head = null;
+	protected Node<E> tail = null;
+
+	public LinList() {
+
+	};
+
+	public LinList(E e) {
+		head = new Node<E>(e);
+		tail = head;
+	};
+
+	public boolean addHead(E e) {
+		if (e == null)
+			return false;
+
+		Node<E> node = new Node<E>(e);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			node.next = head;
+			head = node;
+		}
+		return true;
+	}
+
+	public boolean addTail(E e) {
+		if (e == null)
+			return false;
+
+		Node<E> node = new Node<E>(e);
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			tail.next = node;
+			tail = node;
+		}
+		return true;
+	}
+
+	public boolean insertPos(E e, int position) {
+
+		if ((e == null) || (position < 0))
+			return false;
+
+		Node<E> node = new Node<E>(e);
+
+		if (position == 0) {
+			addHead(e);
+			return true;
+		}
+
+		Node<E> temp = head;
+		for (int i = 0; i < position - 1; i++) {
+			if (temp.next == null) {
+				// System.out.println("Incorrect position!");
+				return false;
+			}
+			temp = temp.next;
+		}
+		node.next = temp.next;
+		temp.next = node;
+		return true;
+	}
+
+	public int size() {
+		int count = 0;
+		Node<E> temp = head;
+		if (temp != null) {
+			count++;
+			while (temp.next != null) {
+				count++;
+				temp = temp.next;
+			}
+		} else {
+			return 0;
+		}
+		return count;
+	}
+
+	public void printList() {
+		if (this.size() == 0) {
+			System.out.println("List is empty!");
+			return;
+		}
+
+		Node<E> temp = head;
+		do {
+			System.out.println(temp.value);
+			temp = temp.next;
+		} while (temp != null);
+
+	}
+
+	public E find(E e) { // returns reference to value==e, null
+								// if not found
+
+		if (head == null) {
+			// System.out.println("List is empty!");
+			return null;
+		}
+
+		Node<E> temp = head;
+
+		while (temp.next != null) {
+			if (temp.value.equals(e)) {
+				return temp.value;
+			}
+			temp = temp.next;
+		}
+
+		return null;
+	}
+
+	public boolean contains(E e) { // true is value exist in list
+		if (head == null) {
+			return false;
+		}
+
+		Node<E> temp = head;
+		while (temp.next != null) {
+			if (temp.value.equals(e)) {
+				return true;
+			}
+			temp = temp.next;
+		}
+		return false;
+	}
+
+	public boolean remove(E e) { // returns true if Node with value==e has
+									// deleted
+		if (head == null) {
+			System.out.println("List is empty!");
+			return false;
+		}
+
+		if (head.value.equals(e)) {
+			head = head.next;
+			return true;
+		}
+
+		Node<E> temp = head;
+
+		while (temp.next != null) {
+			if (temp.next.value.equals(e)) {
+				temp.next = temp.next.next;
+				return true;
+			}
+			temp = temp.next;
+		}
+		return false;
+	}
+
+}
---
+++ src/by/htp/linlist/ListTest.java
@@ -0,0 +1,65 @@
+package by.htp.linlist;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		
+	LinList<String> list = new LinList<String>("first");
+	list.addHead("to Head 1");
+	list.addHead("3");
+	list.addHead("4");
+	list.addHead("5");
+	
+//	System.out.println("head:" +list.head.value);
+//	System.out.println("head:" +list.head.value);
+//	System.out.println("tail:" +list.tail.value);
+//	System.out.println("tail:" +list.tail.value);
+	
+	
+	System.out.println("size="+list.size());
+	
+		
+	list.addTail("6");	
+	System.out.println("tail:" +list.tail.value);
+	
+	System.out.println("size="+list.size());
+//	list.addHead(new Integer ("121")*2);
+	list.printList();
+
+	LinList<Integer> listInt = new LinList<Integer>();
+	
+	listInt.addTail(1);
+	listInt.addTail(2);
+	listInt.addTail(3);
+	listInt.addTail(4);
+	listInt.addTail(5);
+	listInt.addTail(6);
+	
+	System.out.println("size="+listInt.size());
+	listInt.printList();
+	System.out.println("inserting");
+	System.out.println(listInt.insertPos(100, 3));
+//	listInt.addHead(100);
+	listInt.printList();
+	
+	LinList<Integer> listInt2 = new LinList<Integer>();
+	listInt2.addHead(listInt.find(100));
+//	System.out.println("list2");
+//	listInt2.printList();
+//	System.out.println(listInt2.contains(100));
+//	System.out.println(listInt.size());
+//	System.out.println(listInt.contains(1));
+	
+	System.out.println(listInt.remove(100));
+	listInt.printList();
+	
+	listInt2.addHead(1);
+	listInt2.addHead(2);
+	
+	System.out.println(listInt2.remove(0));
+	listInt2.printList();
+	}
+	
+	
+	
+}
